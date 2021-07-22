2d2db330894a39eb9a862829c8346d177cd89795
aishadvitya <aish.advitya@gmail.com>
2021-07-22 14:16:36 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,102 @@
+import os
+import sys
+
+# Flask
+from flask import Flask, redirect, url_for, request, render_template, Response, jsonify, redirect
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+# Some utilites
+import numpy as np
+from util import base64_to_pil
+
+
+# Declare a flask app
+app = Flask(__name__)
+
+
+# You can use pretrained model from Keras
+# Check https://keras.io/applications/
+# or https://www.tensorflow.org/api_docs/python/tf/keras/applications
+
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+# model = MobileNetV2(weights='imagenet')
+
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+# Model saved with Keras model.save()
+MODEL_PATH = 'models/flower_clf.h5'
+
+# Load your own trained model
+model = load_model(MODEL_PATH)
+#model._make_predict_function()          # Necessary
+# print('Model loaded. Start serving...')
+
+
+def model_predict(img, model):
+    img = img.resize((176, 176))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    # x = np.true_divide(x, 255)
+    x = np.expand_dims(x, axis=0)
+
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+    x = preprocess_input(x, mode='tf')
+
+    preds = model.predict(x)
+    return preds
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        # Get the image from post request
+        img = base64_to_pil(request.json)
+
+        # Save the image to ./uploads
+        # img.save("./uploads/image.png")
+
+        # Make prediction
+        preds = model_predict(img, model)
+        print('Prediction is....',preds)  
+        # Process your result for human
+        max_value=np.amax(preds[0])
+        pred_proba = "{:.3f}".format(max_value)    # Max probability
+           # ImageNet Decode
+        max_index=np.where(preds[0]==max_value)[0][0]
+        print('Max index',max_index)
+        classes=['daisy', 'dandelion', 'rose', 'sunflower', 'tulip']
+        
+         
+        result = str(classes[max_index])               # Convert to string
+        result = result.replace('_', ' ').capitalize()
+        
+        # Serialize the result, you can add additional fields
+        return jsonify(result=result,probability=pred_proba)
+
+    return None
+
+
+if __name__ == '__main__':
+    # app.run(port=5002, threaded=False)
+
+    # Serve the app with gevent
+    http_server = WSGIServer(('0.0.0.0', 5000), app)
+    http_server.serve_forever()
